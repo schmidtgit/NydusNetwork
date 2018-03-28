e68315af4484b3d8f746a0b08055b30e6118e4f6
Jens Schmidt <jenh@itu.dk>
2018-03-28 16:12:30 +0200

    Add project files.

---
+++ NydusNetwork/Logging/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace NydusNetwork.Logging
+{
+    public interface ILogger
+    {
+        void LogMessage(object s);
+        void LogSuccess(object s);
+        void LogWarning(object s);
+        void LogError(object s);
+        void LogInfo(object s);
+    }
+}
